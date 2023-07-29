50d400664738ba1e3d21cb0bda9a43750b979c8d
jwo0o0 Kim <jwo0o0358@gmail.com>
2023-07-29 22:44:14 +0900

    Revert "feat: 랜딩 페이지 구현 (#2)"
    This reverts commit 0881c2738c5e6475997b9fc7304a6ca26cd3f528.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { GlobalStyle } from '@styles/global-style';
-import GlobalFonts from '@assets/fonts';
-
 import { Layout } from '@layout/index';
 
 import { LandingPage } from '@pages/Landing';
-import { WritePage } from '@pages/Write';
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
-      <GlobalFonts />
       <Layout>
         <Routes>
           <Route path={'/'} element={<LandingPage />} />
-          <Route path={'/write'} element={<WritePage />} />
         </Routes>
       </Layout>
     </BrowserRouter>
