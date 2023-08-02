8034e96156256372fa05518418593d5a896ad18f
jwo0o0 <jwo0o0358@gmail.com>
2023-08-03 01:02:39 +0900

    fix: 모달 컴포넌트 분리

--- src/components/common/Modal/index.tsx
+++ src/components/common/Modal/index.tsx
@@ -1,6 +1,8 @@
 import { ReactNode } from 'react';
 import { createPortal } from 'react-dom';
-import styled, { css } from 'styled-components';
+
+import { ModalBackdrop } from '@components/common/Modal/ModalBackdrop';
+import { ModalView } from '@components/common/Modal/ModalView';
 
 interface ModalPropsType {
   width?: string;
@@ -10,7 +12,6 @@ interface ModalPropsType {
   setIsOpenModal: React.Dispatch<React.SetStateAction<boolean>>;
   children?: ReactNode;
 }
-
 export const Modal = ({
   isModalOpen,
   setIsOpenModal,
@@ -35,38 +36,3 @@ export const Modal = ({
     </>
   );
 };
-
-const ModalBackdrop = styled.div`
-  background: rgba(0, 0, 0, 0.3);
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 0;
-  bottom: 0;
-`;
-
-const ModalView = styled.div<{
-  width?: string;
-  height?: string;
-  padding?: string;
-}>`
-  display: flex;
-  flex-direction: column;
-
-  position: fixed;
-  ${({ width = '300px', height = '300px' }) => css`
-    left: calc(50vw - ${width} / 2);
-    top: calc(50vh - ${height} / 2);
-    width: ${width};
-    height: ${height};
-  `};
-  z-index: 2000;
-
-  background-color: white;
-
-  border-radius: 8px;
-  border: 1px solid ${({ theme }) => theme.colors.gray_100};
-  ${({ padding = '12px' }) => css`
-    padding: ${padding};
-  `}
-`;
