05e38beebe051569944849a900ae826a7dcae458
jwo0o0 <jwo0o0358@gmail.com>
2023-08-05 00:05:56 +0900

    style: Pretendard woff 파일 추가

--- src/assets/fonts/index.ts
+++ src/assets/fonts/index.ts
@@ -1,39 +1,49 @@
 import { createGlobalStyle } from 'styled-components';
-import PretendardRegular from './Pretendard-Regular.woff2';
-import PretendardBold from './Pretendard-Bold.woff2';
-import PretendardSemiBold from './Pretendard-SemiBold.woff2';
-import PretendardMedium from './Pretendard-Medium.woff2';
-import PretendardExtraBold from './Pretendard-ExtraBold.woff2';
+import PretendardRegular from './woff2/Pretendard-Regular.woff2';
+import PretendardBold from './woff2/Pretendard-Bold.woff2';
+import PretendardSemiBold from './woff2/Pretendard-SemiBold.woff2';
+import PretendardMedium from './woff2/Pretendard-Medium.woff2';
+import PretendardExtraBold from './woff2/Pretendard-ExtraBold.woff2';
 
 export default createGlobalStyle`
 @font-face {
     font-family: "Pretendard";
     src: local("PretendardRegular"),
-    url(${PretendardRegular}) format('woff2');
+    url(${PretendardRegular}) format('woff2'),
+    url("./woff/Pretendard-Regular.woff") format('woff');
     font-style: normal;
+    font-display: swap;
 }
 @font-face {
     font-family: "PretendardBold";
     src: local("PretendardBold"),
-    url(${PretendardBold}) format('woff2');
+    url(${PretendardBold}) format('woff2'),
+    url("./woff/Pretendard-Bold.woff") format('woff');
     font-style: normal;
+    font-display: swap;
 }
 @font-face {
     font-family: "PretendardSemiBold";
     src: local("PretendardSemiBold"),
-    url(${PretendardSemiBold}) format('woff2');
+    url(${PretendardSemiBold}) format('woff2'),
+    url("./woff/Pretendard-SemiBold.woff") format('woff');
     font-style: normal;
+    font-display: swap;
 }
 @font-face {
     font-family: "PretendardMedium";
     src: local("PretendardMedium"),
-    url(${PretendardMedium}) format('woff2');
+    url(${PretendardMedium}) format('woff2'),
+    url("./woff/Pretendard-Medium.woff") format('woff');
     font-style: normal;
+    font-display: swap;
 }
 @font-face {
     font-family: "PretendardExtraBold";
     src: local("PretendardExtraBold"),
-    url(${PretendardExtraBold}) format('woff2');
+    url(${PretendardExtraBold}) format('woff2'),
+    url("./woff/Pretendard-ExtraBold.woff") format('woff');
     font-style: normal;
+    font-display: swap;
 }
 `;
