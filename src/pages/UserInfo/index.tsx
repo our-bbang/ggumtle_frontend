f2019074e4fa322958eb4424109743d2f8316031
jwo0o0 <jwo0o0358@gmail.com>
2023-08-04 13:23:57 +0900

    feat: 유저정보 작성 캐릭터 이미지 및 안내 문구

--- src/pages/UserInfo/index.tsx
+++ src/pages/UserInfo/index.tsx
@@ -2,8 +2,10 @@ import { useEffect } from 'react';
 
 import { ProgressbarWrapper } from '@components/common/Progressbar/ProgressbarWrapper';
 import { Progressbar } from '@components/common/Progressbar';
+import { MainText } from '@components/userinfo/MainText';
 
 import { useProgress } from '@hooks/useProgress';
+import { styled } from 'styled-components';
 
 export const UserInfoPage = () => {
   const updateProgress = useProgress();
@@ -17,13 +19,19 @@ export const UserInfoPage = () => {
   };
 
   return (
-    <>
+    <UserInfoPageContainer>
       <ProgressbarWrapper>
         <Progressbar />
       </ProgressbarWrapper>
 
-      <div>유저 정보 입력</div>
+      <MainText />
       <button onClick={handleClickBtn}>입력 완료</button>
-    </>
+    </UserInfoPageContainer>
   );
 };
+
+const UserInfoPageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
