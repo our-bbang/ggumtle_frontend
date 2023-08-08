2797744f60a5a519893d996e0af42f0653cd38b3
jwo0o0 <jwo0o0358@gmail.com>
2023-08-08 14:18:28 +0900

    feat: axiosInstance 추가

---
+++ src/api/index.ts
@@ -0,0 +1,9 @@
+import axios, { AxiosInstance } from 'axios';
+
+export const client: AxiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
