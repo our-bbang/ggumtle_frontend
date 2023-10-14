12b14aa0a31a2708e1c56e0df4f61a4246d99174
jwo0o0 <jwo0o0358@gmail.com>
2023-10-14 19:32:01 +0900

    feat: 진행률 컴포넌트 작업

---
+++ src/components/home/ProgressCircle.tsx
@@ -0,0 +1,45 @@
+import {
+  CircularProgressbar,
+  CircularProgressbarWithChildren,
+  buildStyles,
+} from 'react-circular-progressbar';
+import './ProgressCircle.css';
+
+import styled from 'styled-components';
+
+export const ProgressCircle = ({
+  percent,
+  index,
+}: {
+  percent: number;
+  index: number;
+}) => {
+  const emojis = ['🍎', '🍊', '🍋', '🍑', '🍓'];
+  return (
+    <ProgressCircleContainer style={{ width: 60, height: 60 }}>
+      <CircularProgressbarWithChildren value={percent}>
+        <FruitIcon>{emojis[index % 5]}</FruitIcon>
+        <ProgressText>{percent}%</ProgressText>
+      </CircularProgressbarWithChildren>
+      {/* <CircularProgressbar value={20} text={`${30}%`} /> */}
+    </ProgressCircleContainer>
+  );
+};
+
+const ProgressCircleContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  right: 18px;
+`;
+
+const FruitIcon = styled.div`
+  font-size: 12px;
+  margin-bottom: 4px;
+`;
+
+const ProgressText = styled.div`
+  font-family: 'PretendardSemiBold';
+  font-size: 12px;
+  color: #eff0f5;
+`;
